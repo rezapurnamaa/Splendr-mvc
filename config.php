aa3bd08619a56165d797879cbd944ab2c19c9fd6
Arief <s0533453@htw-berlin.de>
2015-06-28 14:27:56 +0200

    splendr mvc
    splendr with simple mvc

---
+++ config.php
@@ -0,0 +1,34 @@
+<?php
+
+//set timezone
+date_default_timezone_set('Europe/Berlin');
+
+//site address
+define('DIR','http://localhost/Splendr/');
+define('DOCROOT', dirname(__FILE__));
+
+// Credentials for the local server
+define('DB_TYPE','mysql');
+define('DB_HOST','db.f4.htw-berlin.de');
+define('DB_NAME','_s0533453__products');
+define('DB_USER','s0533453');
+define('DB_PASS','password');
+
+// // Credentials for the local server
+// define('DB_TYPE','mysql');
+// define('DB_HOST','localhost');
+// define('DB_NAME','products');
+// define('DB_USER','root');
+// define('DB_PASS','root');
+
+// Credentials for the HTW server
+// define('DB_HOST','db.f4.htw-berlin.de');
+// define('DB_NAME','_beierm__products');
+// define('DB_USER','beierm');
+// define('DB_PASS','bummelletzter');
+
+//set prefix for sessions
+define('SESSION_PREFIX','splendr_');
+
+//optionall create a constant for the name of the site
+define('SITETITLE','Splendr');
---
+++ models/products_model.php
@@ -0,0 +1,17 @@
+<?php
+
+class Products_Model extends Model {
+
+   public function __construct(){
+      parent::__construct();
+   }
+
+   /**
+   * Gibt die letzten 20 Einträge im Archiv zurück.
+   * @return array Liste aus Produkten mit id, timestamp, name, url, image und price
+   */
+   public function all() {
+      return $this->_db->select('SELECT * FROM products ORDER BY id DESC LIMIT 0, 20');
+   }
+
+}
